1373cea876a67739c630454318e940f6fbb1d424
Austin Chen <chenastn@gmail.com>
2025-04-17 16:36:52 -0400

    formatted profile page

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='user.index'),
     path('lifetime-stats/', views.lifetime_stats, name='user.lifetime_stats'),
+    path('profile/', views.profile, name='user.profile'),  # Current user's profile
+    path('profile/<str:username>/', views.profile, name='user.profile.other'),  # Another user's profile
 ]
